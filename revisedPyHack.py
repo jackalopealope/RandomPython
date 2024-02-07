996b5a897af3ede5b894eb6069fd9d7276c74ea4
jackalopealope <146662223+jackalopealope@users.noreply.github.com>
2024-02-07 11:26:38 -0700

    Update revisedPyHack.py

--- revisedPyHack.py
+++ revisedPyHack.py
@@ -1,29 +1,4 @@
-import os, subprocess, platform
-
-# Define internet connection check (This is an artificial requirement, it is possible to run this script without an internet connection, I just did this for kicks and giggles.)
-def internet_on_firstboot():
-    print("connecting...")
-    try:
-        subprocess.check_output(["ping", "-c", "1", "google.com"], stderr=subprocess.STDOUT, timeout=5)
-        print("Connected")
-        return True
-    except subprocess.CalledProcessError:
-        print("Error. No internet connection present")
-        return False
-    except subprocess.TimeoutExpired:
-        print("Error. Ping timed out.")
-        return False
-
-# This is the same thing as "internet_on_firstboot()" except it is deverbosified so it can check every cycle in the main loop without ruining the illusion by printing to the screen your connection status
-def internet_on_silent():
-    try:
-        subprocess.check_output(["ping", "-c", "1", "google.com"], stderr=subprocess.STDOUT, timeout=5)
-        return True
-    except subprocess.CalledProcessError:
-        return False
-    except subprocess.TimeoutExpired:
-        print("Error. Ping timed out.")
-        return False
+import os, platform, keyboard, time
 
 # This is made to make it easier to run a command in the host's command interpreter. The main ones being: 
 # (I will not count virtualizing these shells to make an operating system compatible with these shells, that would make this list pointless.
@@ -31,6 +6,7 @@ def internet_on_silent():
 # [Bash - GNU/Linux + MacOS Mojave and earlier]
 # [Powershell - Windows Xp and later + Optional to download on most decently modern linux distros]
 # [Zsh - MacOS Catilina and earlier]
+
 def command(c):
     os.system(c)
 
@@ -47,40 +23,41 @@ def setup():
     if runningOS == "Linux":
         command(aliasstring)
     # This is a Windows only thing, if you know how, feel free to push an update that does this for bash based shells.
-    if runningOS == "nt":
+    if runningOS == "Windows":
         command("color 0a")
      
  
     # This is where the magic happens, it is what you will see when you run this program.
 def Hack():
-    # Variable is set again inside the function.
-    IsConnected = internet_on_silent()
+    Level = 7
+    runningOS = platform.system()
+    if runningOS == "Windows":
+        command("start /MAX cmd /k \"color 0a && cd \ && tree && dir && tree && dir && tree && dir && tree && dir && tree && dir && tree && dir && tree && dir\"")
+        for i in range(Level):
+            command("start cmd /c \"color 0a && cd \ && tree && dir && tree && dir && tree && dir && tree && dir && tree && dir && tree && dir && tree && dir\"")
+            i = i+1
+    elif runningOS == "Linux":
+        command("")
     
-    # Tests connection before next cycle
     while True:
-        IsConnected = internet_on_silent()
-        # This is why I did that unreadable aliasing up above.
-        if IsConnected == True:
+            # This is the command that is not installed by default I did that unreadable aliasing up above.
             command("tree")
-            # Extra stuff to list stuff in a different way
             runningOS = platform.system()
-            if runningOS == "nt":
+            if runningOS == "Windows":
                 command("Dir -r")
-            return True
-        else:
-            print("Disconnected...")
-            break
+            
+        
 
 # Main loop runs "Hack()" until connection is lost.     
 def main():
     try:
         Hack()
-        
-        
     except KeyboardInterrupt:
-        print("Disconnected...")
+        print("\n" * 10000)
+        print("Acess Granted...")
         input("Press any key to exit")
         exit()
 
+setup()
 main()
 
